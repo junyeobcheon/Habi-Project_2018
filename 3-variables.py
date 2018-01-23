e724811e333b6ff2191d143d05a5dda39b689d8d
junyeob cheon <tgo5269@gmail.com>
2018-01-23 20:44:06 +0900

    first commit

---
+++ 3-variables.py
@@ -0,0 +1,7 @@
+a = 192 #integer(정수) type
+b = 192. #float(실수) type
+c = "192" #string(문자열) type
+
+print("a의 타이프는:", "\t", type(a))
+print("b의 타이프는:", "\t", type(b))
+print("c의 타이프는:", "\t", type(c))
