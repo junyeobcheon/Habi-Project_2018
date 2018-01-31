acae742f1ccf21be96de644774bad42efe1d189d
junyeob cheon <tgo5269@gmail.com>
2018-01-31 19:01:22 +0900

    New

---
+++ 2018-1-31-variables2.py
@@ -0,0 +1,7 @@
+variable_1 = input("당신의 이름은 무엇입니까?")
+variable_2 = input("당신의 나이는 얼마인가요?")
+print("Hello,", variable_1)
+print("Your age:", variable_2) # variable_1과 variable_2에 ""를 씌우면 안되는 이유는 ???
+print("당신이 입력한 이름의 type는", type(variable_1))
+print("당신이 입력한 나이의 type는", type(variable_2))
+#type(variable_1)과 type(variable_2)에는 왜 "" 를 안 씌울까?
