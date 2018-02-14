5dc95975d87911201f9707f9cf0af59efe5e78df
junyeob cheon <tgo5269@gmail.com>
2018-02-14 17:03:39 +0900

    New

---
+++ 2018-2-14-if_ex.py
@@ -0,0 +1,7 @@
+user_input = int(input("숫자를 입력하세요."))
+if (user_input > 30):
+    print("You are not accepted.")
+elif (user_input < 30 and user_input > 20):
+    print("Hello, World")
+else:
+    print("Welcome to our club !")
