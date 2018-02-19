e166839695aeed045cddb30f5fca82bde29fd095
junyeob cheon <tgo5269@gmail.com>
2018-02-19 17:13:01 +0900

    new

--- epitaxy.py
+++ epitaxy.py
@@ -9,19 +9,16 @@ contents = str(f.readlines())
 result1 = re.findall(r"(<d>)([\s\S]+?)(</d>)", contents)
 result2 = re.findall(r"(<con>)([\s\S]+?)(</con>)", contents)
 result3 = re.findall(r"(<eval>)([\s\S]+?)(</eval>)", contents)
+for i in range(len(result1)):
+    line1.append(result1[i][1])
+    line2.append(result2[i][1])
+    line3.append(result3[i][1])
+f.close()
 
-print(len(result1))
-
-# result1 = result1[::-1]
-# result2 = result2[::-1]
-# result3 = result3[::-1]
-# f.close()
-# for i in range(len(result1)):
-#     print(result1[i][1])
-# f = open("coding_record.txt", "w", encoding = "utf-8")
-# for i in range(len(result1)):
-#     if (result2[i][1] != "jump"):
-#         f.write(str(result1[i][1]) + "\n")
-#         f.write(str(result2[i][1]) + "\n")
-#         f.write(str(result3[i][1]) + "\n")
-# f.close()
+f = open("coding_record.txt", "w", encoding = "utf-8")
+for i in range(len(line1)):
+    f.write(str(line1[i]) + "\n")
+    f.write("내용: " + str(line2[i]) + "\n")
+    f.write("평가: " + str(line3[i]) + "\n")
+    f.write("\n")
+f.close()
